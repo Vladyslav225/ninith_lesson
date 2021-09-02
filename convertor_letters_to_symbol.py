316ef92b962a31fca0cc833b94ff0d8513c5fd1e
Yarynovskiy Vladyslav <yvv.work@gmail.com>
2021-09-02 17:01:14 +0300

    .

--- convertor_letters_to_symbol.py
+++ convertor_letters_to_symbol.py
@@ -1,99 +1,3 @@
-# Converter
-
-
-# list_letters = []
-
-# width_line = 10
-
-
-# def first_line():
-
-#      line_0 = '#' * width_line
-
-#      return line_0
-
-# def second_line():
-
-#      line_1 = '##'
-
-#      for _ in range(6):
-#           line_1 += '.'
-     
-#      line_1 += '##'
-
-#      return line_1
-
-# def third_line():
-
-#      line_2 = '##'
-
-#      for _ in range(8):
-#           line_2 += '.'
-
-#      return line_2
-
-
-
-# def func_v():
-
-#      for _ in range(6):
-#           line = second_line()
-#           print(line)
-
-#      line = first_line()
-#      print(line, '\n')
-
-
-# def func_l():
-
-#      for _ in range(6):
-#           line = third_line()
-#           print(line)
-
-#      line = first_line()
-#      print(line, '\n')
-
-# def func_a():
-
-#      line = first_line()
-#      print(line)
-
-#      for _ in range(2):
-#           line = second_line()
-#           print(line)
-     
-#      line = first_line()
-#      print(line)
-
-#      for _ in range(3):
-#           line = second_line()
-#           print(line)
-#      print()
-
-# def func_d():
-
-#      line = first_line()
-#      print(line)
-
-#      for _ in range(5):
-#           line = second_line()
-#           print(line)
-     
-#      line = first_line()
-#      print(line)
-
-# line_dict = {
-#      'v': func_v(),
-#      'l': func_l(),
-#      'a': func_a(),
-#      'd': func_d()
-#      }
-
-
-
-
-
-
 width_line = 10
 
 list_letters = 'v'
@@ -125,6 +29,8 @@ def third_type_line():
      return line_2
 
 def func_v():
+
+
 
      line_v_0 = second_type_line()
      line_v_1 = second_type_line()
--- hw_decorator-figure.py
+++ hw_decorator-figure.py
@@ -1,37 +1,75 @@
+list_lines_symbol = []
+
+
 def star_converter(_str, size):
 
      res = _str * size
      return res
 
+def sharpe_converter(_str, size):
+     
+     symbol = '**'
 
+     for _ in range(size):
+          symbol += '#'
 
-def sharpe_converter():
-     pass
+     symbol += '**'
 
-def procent_converter():
-     pass
+     return symbol
 
-def stepper_converter():
-     pass
+def procent_converter(_str, size):
 
-def at_converter():
-     pass
+     symbol = '**##'
 
-dict_converters = {
-     'a': star_converter,     # '*'
-     'b': sharpe_converter,   # '#'
-     'c': procent_converter,  # '%'
-     'd': stepper_converter,  # '^'
-     'e': at_converter        # '@'
-}
+     for _ in range(size):
+          symbol += '%'
 
-line_1 = star_converter('*', 21)
-line_2 = star_converter('#', 21)
-line_3 = star_converter('%', 21)
-line_4 = star_converter('', 21)
-line_5 = star_converter('*', 21)
-line_6 = star_converter('*', 21)
-line_7 = star_converter('*', 21)
-line_8 = star_converter('*', 21)
-line_9 = star_converter('*', 21)
-print(line_1)
+     symbol += '##**'
+
+     return symbol
+
+def stepper_converter(_str, size):
+     
+     symbol = '**##%%'
+
+     for _ in range(size):
+          symbol += '^'
+
+     symbol += '%%##**'
+
+     return symbol
+
+def at_converter(_str, size):
+     symbol = '**##%%^^'
+
+     for _ in range(size):
+          symbol += '@'
+
+     symbol += '^^%%##**'
+
+     return symbol
+
+def creating_picture():
+     line_1 = star_converter('*', 20)
+     line_2 = sharpe_converter('#', 16)
+     line_3 = procent_converter('%', 12)
+     line_4 = stepper_converter('^', 8)
+     line_5 = at_converter('@', 4)
+     line_6 = stepper_converter('^', 8)
+     line_7 = procent_converter('%', 12)
+     line_8 = sharpe_converter('#', 16)
+     line_9 = star_converter('*', 20)
+     
+     res_picture = line_1 + '\n' + line_2 + '\n' + line_3 + '\n' + line_4 + '\n' + line_5 + '\n' + line_6 + '\n' + line_7 + '\n' + line_8 + '\n' + line_9
+
+     return list_lines_symbol.append(res_picture)
+
+creating_picture()
+
+for line_symbol in list_lines_symbol:
+     print(line_symbol, '\n')
+
+with open('convert.txt', 'a') as convert:
+     for line_symbol in list_lines_symbol:
+          convert.write(line_symbol + ('\n'))
+          convert.close
