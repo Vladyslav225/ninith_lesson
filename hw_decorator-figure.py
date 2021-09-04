4deb2b9605cf039c3fd99cebce1262e3c28a5e22
Yarynovskiy Vladyslav <yvv.work@gmail.com>
2021-09-05 00:33:14 +0300

    .

--- hw_decorator-figure.py
+++ hw_decorator-figure.py
@@ -1,3 +1,6 @@
+import time
+
+
 list_lines_symbol = []
 
 
@@ -49,18 +52,29 @@ def at_converter(_str, size):
 
      return symbol
 
+
+def decorator_timing(creating_picture):
+
+     def timing(*args, **kwargs):
+
+          start = time.time()
+          res = creating_picture(*args, **kwargs)
+          print(time.time() - start)
+
+          return res
+
+     return timing
+
+@decorator_timing
+
 def creating_picture():
      line_1 = star_converter('*', 20)
      line_2 = sharpe_converter('#', 16)
      line_3 = procent_converter('%', 12)
      line_4 = stepper_converter('^', 8)
      line_5 = at_converter('@', 4)
-     line_6 = stepper_converter('^', 8)
-     line_7 = procent_converter('%', 12)
-     line_8 = sharpe_converter('#', 16)
-     line_9 = star_converter('*', 20)
      
-     res_picture = line_1 + '\n' + line_2 + '\n' + line_3 + '\n' + line_4 + '\n' + line_5 + '\n' + line_6 + '\n' + line_7 + '\n' + line_8 + '\n' + line_9
+     res_picture = line_1 + '\n' + line_2 + '\n' + line_3 + '\n' + line_4 + '\n' + line_5 + '\n' + line_4 + '\n' + line_3 + '\n' + line_2 + '\n' + line_1
 
      return list_lines_symbol.append(res_picture)
 
